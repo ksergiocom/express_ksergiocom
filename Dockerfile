0643a97cb9dcbd3eaeee2a45b45db8dd7d70fdff
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-13 00:52:23 +0100

    Hola!

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usar una imagen base de Node
+FROM node:20
+
+# Establecer el directorio de trabajo en el contenedor
+WORKDIR /usr/src/app
+
+# Copiar el archivo package.json y package-lock.json (si está disponible)
+COPY app/package*.json ./
+
+# Instalar las dependencias del proyecto
+RUN npm install
+
+# Copiar los archivos del proyecto
+COPY app/ .
+
+# Exponer el puerto que utiliza tu aplicación (docker-compose.yml sobreescribe este comando)
+EXPOSE 3000
+
+# Comando para arrancar la aplicación usando npm start (docker-compose.yml sobreescribe este comando)
+CMD ["npm", "start"]
---
+++ app/src/index.js
@@ -0,0 +1,59 @@
+/*
+    Si, esta todo metido en un único archivo.
+    Es un proyecto tan pequeño que ni me he molestado :)
+*/
+
+require('express-async-errors')
+const marked = require('marked')
+const sanitizeHtml = require('sanitize-html')
+const express = require('express')
+const mongo = require('mongodb')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const MONGO_URI = process.env.MONGO_URI
+const client = new mongo.MongoClient(MONGO_URI)
+const db = client.db(process.env.MONGO_DB)
+
+app.set('views', __dirname+'/views')
+app.set('view engine', 'pug')
+
+app.use('/static', express.static(__dirname+'/static'))
+
+app.all('/', async (req,res) => {
+    const posts = await db.collection('posts').find().toArray()
+    return res.render('index.pug', {posts})
+})
+
+app.all('/:slug', async (req,res) => {
+    const {slug} = req.params
+    const post = await db.collection('posts').findOne({slug})
+    if(!post) return res.render('404.pug')
+
+    const fecha = new Date(post.fecha).toLocaleDateString('es')
+    const parsedPost = sanitizeHtml(marked.parse(post.post),{
+        allowedTags:['img','h1','h2','h3','strong','i','a','strong','ul','li','ol','code','pre']
+    })
+    return res.render('post.pug',{post, parsedPost, fecha})
+})
+
+app.all('*', (req, res) => res.render('404.pug'))
+
+app.use((err, req, res, next) => {
+    return res.render('500.pug')
+})
+
+const startServer = async () => {
+    try {
+        await client.connect()
+        console.log("Conectado a Mongo exitosamente.")
+        app.listen(PORT, () => {
+            console.log(`Servidor funcionando en: http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log("Algo salió mal!!! \n", error)
+    }
+}
+
+startServer()
